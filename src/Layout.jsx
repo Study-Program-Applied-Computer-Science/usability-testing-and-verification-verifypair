db252eec83ecd3b2dcabcc1928e1e7fef215ef93
manuj55 <manuj6885@gmail.com>
2025-02-26 22:09:33 +0100

    removed material ui and add bootstrap

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,10 @@
-import Grid from '@mui/material/Grid2';
 import PropTypes from 'prop-types';
 
 
 const Layout = ({ children }) => {
     return (
-        <Grid container>
-            <Grid item xs={12}>
-                {children}
-            </Grid>
-        </Grid>
+        <>
+        </>
     )
 }
 Layout.propTypes = {
